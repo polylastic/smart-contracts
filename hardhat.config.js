36946f289800bff1c2400dc13db1394031aa3828
polylastic-dev <development@polylastic.io>
2021-05-12 23:24:29 +0200

    Add testnet rpc configuration.

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,6 +20,12 @@ module.exports = {
     local: {
       url: 'http://localhost:8545',
     },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [process.env.PK]
+    },
     mainnet: {
       url: "https://bsc-dataseed.binance.org/",
       chainId: 56,

616d4cd3f6d3d7807e50e61c595001f24dfd7828
polylastic-dev <development@polylastic.io>
2021-05-17 12:51:39 +0200

    Add migration for Airdrop and TokenSwapPortal contracts.

--- scripts/deploy_airdrop_tokenswap.js
+++ scripts/deploy_airdrop_tokenswap.js
@@ -13,6 +13,15 @@ async function main() {
     await airdrop.deployed();
     console.log("Airdrop deployed to: ", airdrop.address);
     saveContractAddress(hre.network.name, 'Airdrop', airdrop.address);
+
+    const TokenSwap = await hre.ethers.getContractFactory('TokenSwapPortal');
+    const tokenSwapPortal = await TokenSwap.deploy(
+        getSavedContractAddresses()[hre.network.name]['PolylasticToken'],
+        getSavedContractAddresses()[hre.network.name]['PolylasticTokenV2']
+    )
+    await tokenSwapPortal.deployed();
+    console.log('TokenSwap portal is deployed to: ',tokenSwapPortal.address);
+    saveContractAddress(hre.network.name, 'TokenSwapPortal', tokenSwapPortal.address)
 }
 
 
